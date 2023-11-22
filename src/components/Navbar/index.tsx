b2f21135307431d74fc3bf034a75c87d30a916c4
Mohammed Benkhattab <mobenkhattab@gmail.com>
2023-11-22 18:54:21 +0100

    add navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,8 @@
+export default () => {
+    return (
+        <nav className="h-[70px]  w-full flex items-center justify-between px-2">
+            <h1 className="text-2xl font-kranky">GoaluinTM</h1>
+            <button className="border-2 rounded-md border-theme-red px-4 py-[2px] font-semibold text-theme-red">Login</button>
+        </nav>
+    );
+}
